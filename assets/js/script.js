cc0658b0b3b3f23dc26e755b45d3b16f694376a9
quentinphe <prudhommeq23@gmail.com>
2023-12-20 10:58:48 +0100

    first Landing Page

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+     document.addEventListener("DOMContentLoaded", function () {
+        var logo1 = document.querySelector(".first");
+        var logo2 = document.querySelector(".second");
+        var fullLogo = document.querySelector(".full-logo");
+        var desc = document.querySelector(".desc");
+
+        
+        // Ajoute la classe de l'animation après un délai
+        setTimeout(function () {
+            logo1.classList.add("appearAndZoom");
+
+            // Déclenche la deuxième animation après la première
+            logo1.addEventListener("animationend", function () {
+                logo1.classList.add("appearAndMoveLeft");
+                logo2.classList.add("appearFromleft")
+
+                logo2.addEventListener("animationend", function () {
+                    fullLogo.classList.add("moveUp");
+                    desc.classList.add("moveUp");
+                });
+            });
+        }, 500); 
+    });
